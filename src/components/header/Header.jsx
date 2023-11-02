e17ae3fcbc158abb721692f9e09b5a9b2ce51e55
“rakibul8226” <rakibul8226@gmail.com>
2023-11-03 04:00:41 +0600

    header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
-
+import profile from'../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <div>
-            <h1 className=' text-6xl bg-red-400'>Knowledge Cafe</h1>        
+        // <div className='flex justify-between items-center p-5 m-5 bg-gray-700'>
+        <div className='flex justify-between items-center p-5 m-5 border-b-2 border-gray-400'>
+            <h1 className=' text-6xl '>Knowledge Cafe</h1>        
+            <img src={profile} alt="" />
         </div>
     );
 };
